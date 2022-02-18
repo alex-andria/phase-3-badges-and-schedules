ce68f997c61bdfbe630849c1719b9b0863a9991f
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2022-02-18 01:41:13 -0500

    commit tree 09a21759ddf6d4b63474e0a7db7a602e3919db2c

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator namesArray
+    namesArray.map{ |name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms namesArray
+    namesArray.map.with_index(1) do |name, room_num|
+        "Hello, #{name}! You'll be assigned to room #{room_num}!"
+      end
+end
+
+def printer namesArray
+    batch_badge_creator(namesArray).map{|arr| puts arr}
+    assign_rooms(namesArray).map{|arr| puts arr}
+end
